3dcd0c007ae4002c1c845243411f46d0ab87a538
xhlazz <160189425+xhlazz@users.noreply.github.com>
2025-03-13 16:51:49 -0400

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-fetch('/.netlify/functions/fetchClub')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        document.getElementById('club-name').innerText = data.name;  // Example usage
-    })
-    .catch(error => console.error("Error fetching club data:", error));
